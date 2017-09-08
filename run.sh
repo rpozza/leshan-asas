204ad1c3e0e2c017f37e3b9d8441a32ff962c9b6
Riccardo <r.pozza@surrey.ac.uk>
2017-09-08 11:56:34 +0000

    Tuned Heap

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-/usr/bin/java -Xmx7168m -Xms5120m -jar /home/ubuntu/leshan/leshan-server-demo/target/leshan-server-demo-1.0.0-SNAPSHOT-jar-with-dependencies.jar
+/usr/bin/java -Xmx3g -Xms3g -jar /home/ubuntu/leshan/leshan-server-demo/target/leshan-server-demo-1.0.0-SNAPSHOT-jar-with-dependencies.jar
+# -Xmx7168m -Xms5120m on hpc
